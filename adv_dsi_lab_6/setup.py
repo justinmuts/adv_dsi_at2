e4bf8029ebe1da932514f10241f942518e964179
justinmuts <37526487+justinmuts@users.noreply.github.com>
2022-03-19 17:14:36 +1100

    init

---
+++ adv_dsi_lab_6/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Lab 6 week 6 Neural networks',
+    author='justin',
+    license='',
+)
